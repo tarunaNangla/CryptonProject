a7da7e899037084e59e0c2cb213ff7cbcc398b5e
tarunaNangla <tarunanangla131@gmail.com>
2023-04-05 15:32:10 +0530

    update in cal question

--- question3/index.js
+++ question3/index.js
@@ -1,25 +1,40 @@
 
-                  // taking example of "+" Plus
-
-
-function cal(a,b,opp){
-    
-    console.log(`what is ${a} ${opp}  ${b}`)
-    switch(opp){
-        case '+':
-            return a + b;
-            case '-':
-                return a - b;
-                case '*':
-                    return a * b;
-                    case '/':
-                        return a /b;
-                        default:
-                            return 'not valid';
-                        }
-                    }
-let a=6,b=6;
-let opp="+";
-
-console.log(cal(a,b,opp))
-
+                 
+                  function cal(a,b,opp){
+                    //converting those string value into number type; 
+                    var x=(+a);
+                    var y=(+b)
+                    
+                      console.log(`what is ${x} ${opp}  ${y}`)
+                  
+                    
+                      switch(opp){
+                          case 'plus':
+                              return `${x} plus ${y} is ${x+y}`;
+                              case 'minus':
+                                  return `${x} minus ${y} is ${x-y}`;
+                                  case 'multiplied':
+                                      return `${x} multiplied ${y} is ${x*y}`;
+                                      case 'divided':
+                                          return `${x} divided ${y} is ${x/y}`;
+                                          default:
+                                              return 'not valid';
+                                          }
+                                      }
+                  // Taking input from the user
+                  var a = prompt(`enter value of a `);
+                  var b = prompt(`enter value of b `);
+                  
+                  
+                  //add some basic instructions for user to understand how to use this console.log 
+                  console.log("enter any one operation which you want to perform")
+                  console.log("enter 'plus' for addition");
+                  console.log("enter 'minus' for subtraction");
+                  console.log("enter 'multiplied' for multiplication");
+                  console.log("enter 'divided' for division")
+                  
+                  
+                  //taking input from the user to perform which operation here
+                  var opp=prompt(`enter operation here `); ;
+                  
+                  console.log(cal(a,b,opp));
--- question5/OrderSummary.js
+++ question5/OrderSummary.js
@@ -1,9 +1,26 @@
+// console.log("yo")
 
-const fs=require("fs");
 
+const fs=require('fs');
+const csv=require('csv-parser');
 
-//dataCreate a node js program "OrderSummary" which reads a csv file data and
-// prints the total and summary of orders.
-// Csv File Format:
-// OrderDate,Code,Account,TotalIncl,TotalExcl,VatAmount
-// 12/09/2023,ORD0002032,Ram,28.08,26,2.08
+const inputFile = 'orders.csv';
+
+let ordersCount = 0;
+let ordersTotal = 0;
+let vatTotal = 0;
+
+fs.createReadStream(inputFile)
+  .pipe(csv())
+  .on('data', (row) => {
+    const totalIncl = parseFloat(row['TotalIncl']);
+    const vatAmount = parseFloat(row['VatAmount']);
+    ordersCount++;
+    ordersTotal += totalIncl;
+    vatTotal += vatAmount;
+  })
+  .on('end', () => {
+    console.log(`No of orders: ${ordersCount}`);
+    console.log(`Total: ${ordersTotal.toFixed(2)}`);
+    console.log(`Vat: ${vatTotal.toFixed(2)}`);
+  });
